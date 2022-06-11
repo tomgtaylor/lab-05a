5e3d9f830833b5995b591498b2e233e1a9b351ff
Tom Taylor <tomgtaylor@yahoo.com>
2022-06-10 18:02:34 -0700

    Finished Prolem 3 on the sumAndMultiply branch

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-    let number = a * b
+    let number = a * b;
     return[number, 'The product of ' + a + ' and ' + b + ' is ' + number + '.'];
 }
 
@@ -51,11 +51,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+    let firstSum = sum(a, b)[0];
+    console.log(firstSum);
+    let totalSum = sum(c, firstSum)[0];
+    let firstMultiply = multiply(a, b) [0]
+    let totalMultiply = multiply(c, firstMultiply)[0];
 
+    return [totalSum, totalMultiply, `${a} and ${b} and ${c} sum to ${totalSum}.`, `The product of ${a} and ${b} and ${c} is ${totalMultiply}.`];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
